9883a7b8198a1359a908993672d82692fca0a255
tomasrl18 <tomaslopezraigal@gmail.com>
2023-10-16 17:48:17 +0200

    Change the api and show the results

--- src/api.js
+++ src/api.js
@@ -1,23 +1,23 @@
 const OPTIONS = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5",
-    "X-RapidAPI-Host": "ip-geolocation-and-threat.p.rapidapi.com",
-  },
+    method: "GET",
+    headers: {
+        'X-RapidAPI-Key': 'c2d0948c5dmsh8ad23d308bb42f0p102cffjsn6c2486190df9',
+        'X-RapidAPI-Host': 'ip-reputation-geoip-and-detect-vpn.p.rapidapi.com'
+    }
 };
 
 const fetchIpInfo = (ip) => {
   return fetch(
-    `https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`
+    `https://ip-reputation-geoip-and-detect-vpn.p.rapidapi.com/?ip=${ip}`, OPTIONS
   )
     .then((res) => res.json())
     .catch((err) => console.error(err));
 };
 
-const form = document.getElementById("#form");
-const input = document.getElementById("#ip");
-const submit = document.getElementById("#submit");
-const results = document.getElementById("#results");
+const form = document.getElementById("form");
+const input = document.getElementById("ip");
+const submit = document.getElementById("submit");
+const results = document.getElementById("results");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -29,6 +29,10 @@ form.addEventListener("submit", async (event) => {
   submit.setAttribute("aria-busy", "true");
 
   const ipInfo = await fetchIpInfo(value);
+
+  if (ipInfo) {
+    results.innerHTML = JSON.stringify(ipInfo, null, 2);
+  }
 
   submit.removeAttribute("disabled");
   submit.removeAttribute("aria-busy");
