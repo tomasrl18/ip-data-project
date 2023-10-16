bd3f94a11e1a0ce0f65d243cdcdbe45cd0caaa16
tomasrl18 <tomaslopezraigal@gmail.com>
2023-10-16 17:18:30 +0200

    Format file and do a little loader

--- src/api.js
+++ src/api.js
@@ -1,26 +1,34 @@
 const OPTIONS = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5',
-        'X-RapidAPI-Host': 'ip-geolocation-and-threat.p.rapidapi.com'
-    }
-}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5",
+    "X-RapidAPI-Host": "ip-geolocation-and-threat.p.rapidapi.com",
+  },
+};
 
-const fetchIpInfo = ip => {
-    return fetch(`https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`)
-        .then(res => res.json())
-        .catch(err => console.error(err));
-}
+const fetchIpInfo = (ip) => {
+  return fetch(
+    `https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`
+  )
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
 
-const form = document.querySelector('#form');
-const input = document.querySelector('#ip');
-const submit = document.querySelector('#submit');
+const form = document.getElementById("#form");
+const input = document.getElementById("#ip");
+const submit = document.getElementById("#submit");
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    const {value} = input;
-    if (!value) return;
+  const { value } = input;
+  if (!value) return;
 
-    const ipInfo = await fetchIpInfo(value);
+  submit.setAttribute("disabled", "");
+  submit.setAttribute("aria-busy", "true");
+
+  const ipInfo = await fetchIpInfo(value);
+
+  submit.removeAttribute("disabled");
+  submit.removeAttribute("aria-busy");
 });
